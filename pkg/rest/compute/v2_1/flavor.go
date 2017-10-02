435348054f3afdfa707cc74ba70be3845b3bb3e5
Daniel P. Berrange <berrange@redhat.com>
2017-10-02 14:29:12 +0100

    Fixed group name for project/user accounts
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- pkg/rest/compute/v2_1/flavor.go
+++ pkg/rest/compute/v2_1/flavor.go
@@ -26,7 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
-	k8sv1 "k8s.io/client-go/pkg/api/v1"
 
 	"github.com/dicot-project/dicot-api/pkg/api/compute"
 	"github.com/dicot-project/dicot-api/pkg/api/compute/v1"
